3360e9c4acc4b4c95964e34dab6e1c5627978749
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2024-02-22 16:13:41 +0300

    updated.

---
+++ Week1-Exercises/primeNumberFinder/README.md
@@ -0,0 +1,4 @@
+# Prime Number Finder
+- Java program that prints prime numbers between 1 and 100 on the screen.
+### Sample output:
+
--- Week1-Exercises/primeNumberFinder/src/PrimeNumber.java
+++ Week1-Exercises/primeNumberFinder/src/PrimeNumber.java
@@ -1,8 +1,20 @@
 public class PrimeNumber {
     void func(){
-        System.out.print("2 ");
-        for (int i = 3; i <= 100; i++) {
-            if(i%2==1){
+
+        boolean asal;
+        // belirlenen aralıktaki sayılar burada bastırılır.
+        for (int i = 2; i <= 100; i++) {
+            asal = true;
+            // sayılar burada kontrol edilir.
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    asal = false;
+                    break;
+                    // sayı asal değilse false döndürülür ve aşağıdaki if'e girilmeden döngü devam eder.
+                }
+            }
+            // sayı asal ise burada bastırılır.
+            if (asal) {
                 System.out.print(i + " ");
             }
         }
