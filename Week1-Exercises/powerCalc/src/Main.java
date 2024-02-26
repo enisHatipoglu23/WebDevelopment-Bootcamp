0203297241e2a941b0fd7ea4bd8f0b3a25dd7788
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2024-02-26 12:30:17 +0300

    updated to recursive

--- Week1-Exercises/powerCalc/src/Main.java
+++ Week1-Exercises/powerCalc/src/Main.java
@@ -6,11 +6,14 @@ public class Main {
 
     // calculation method.
     static int power(int a, int b){
-        int result = 1;
-        for (int i = 1; i <= b ; i++) {
-            result *= a;
+        int result;
+        if(b == 0){
+            result = 1;
+        }else{
+            result = a * power(a, b-1);
         }
         return result;
+
     }
     // main method
     public static void main(String[] args){
