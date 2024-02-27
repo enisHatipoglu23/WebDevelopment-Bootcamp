ebd5760612a4c80ce20e3647a2d58eaa5e5f06e5
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2024-02-27 18:43:18 +0300

    codes updated

---
+++ Week1-Exercises/obs/src/Course.java
@@ -0,0 +1,56 @@
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int perfNote;
+    double result;
+
+    // Constructor method
+    Course(String name, String code, String prefix, Teacher teacher){
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+        this.perfNote = 0;
+        this.teacher = teacher;
+        this.result = note*0.8 + perfNote*0.2;
+    }
+
+
+    // teacher checker method
+    void addTeacher(){
+        if(teacher.branch.equals(this.prefix)){
+            System.out.println("Successfuly added !\nTeacher's Info: ");
+            printTeacher();
+        }else{
+            System.out.println("This course's prefix is " + this.prefix + " please pay attention to teacher's branch ! ");
+        }
+    }
+
+    // prints teacher if its suitable
+    void printTeacher(){
+        if(teacher != null){
+            System.out.println("The professor of "+ this.name + " is " + teacher.name + ".");
+        }else{
+            System.out.println("Course doesn't has any teacher yet.");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
