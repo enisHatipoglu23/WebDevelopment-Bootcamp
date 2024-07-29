530126d24e5d780099a97fd0fedb53a28f2e8807
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2024-07-29 11:47:50 +0300

    updated

--- Frontend/Week-7/DrumKit/js/script.js
+++ Frontend/Week-7/DrumKit/js/script.js
@@ -28,7 +28,6 @@ function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
-
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 keys.forEach(key => key.addEventListener('click', playSound));
